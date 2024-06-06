7063e1441820a785359743aacbf721c0d004494e
HoreichFawad <168718621+HoreichFawad@users.noreply.github.com>
2024-06-06 14:43:03 +0200

    Updated with W5500

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,20 @@
 #include "level_sensor.hpp"
 #include "max31865.hpp"
 #include "max31865_continuous.hpp"
+#include "EthernetInterface.h"
+#include "w5500_header.hpp"
+#include "testw5500.hpp"
+#include "w5500example.hpp"
+
 
 int main(void)
 {
-  runFDC1004Test();
-  levelSensor();
-  MAX31865_Continuous maxObj;
-  maxObj.Run();
+  // printf("STM 32 new board");
+  // runFDC1004Test();
+  // levelSensor();
+  // runW5500Test();
+  serverExampleTCP();
+  // clientExampleTCP();
+  // MAX31865_Continuous maxObj;
+  // maxObj.Run();
 }
